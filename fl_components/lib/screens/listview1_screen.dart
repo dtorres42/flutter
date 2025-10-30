03e703d85a8685ebf711c1756d99e9dfd058348f
dtorres42 <davidtl20066gmail.com>
2025-10-30 09:24:20 +0100

    Menu

---
+++ fl_components/lib/screens/listview1_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Listview1Screen extends StatelessWidget {
+  final options = const ['Piplup', 'Lugia', 'Charmander', 'Lucario'];
+
+  const Listview1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Listview tipo 1'),
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView(
+        children: [
+          ...options.map((e) => ListTile(
+                  trailing: Icon(Icons.arrow_forward_ios_outlined),
+                  title: Text(e),
+                ),).toList(),
+
+          /*
+          */
+        ],
+      ),
+    );
+  }
+}
